d2de40f1dbcf82b5c780a0abc83bf82560fd2873
Mashhurakhon <mashhurakhon03@gmail.com>
2025-03-19 11:58:45 +0500

    sidebar

---
+++ src/app/modules/user-module/components/sidebar/sidebar.component.ts
@@ -0,0 +1,24 @@
+// import { Component } from '@angular/core';
+// import { MatDrawerMode, MatSidenavModule } from '@angular/material/sidenav';
+// import { MatButtonModule } from '@angular/material/button';
+// import { MatRadioModule } from '@angular/material/radio';
+
+// import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+//  @Component({
+//   selector: './app-sidebar',
+//   standalone: true,
+//   imports: [
+//     MatSidenavModule,
+//     MatButtonModule,
+//     MatRadioModule,
+//     FormsModule,
+//     ReactiveFormsModule    
+//   ],
+//   templateUrl: './sidebar.component.html',
+//   styleUrl: './sidebar.component.scss'
+// })
+// export class SidebarComponent {
+//   mode = new FormControl('over' as MatDrawerMode);
+//   hasBackdrop = new FormControl(null as null | boolean);
+//   position = new FormControl('start' as 'start' | 'end');
+// }
